d66a24bb1d16b58386c01b8ea7e71132c8bfc615
mryancox <m.ryan.cox@gmail.com>
2019-03-01 14:51:23 -0700

    minor changes to author to fix error, ready to merge

--- src/Author.java
+++ src/Author.java
@@ -96,7 +96,7 @@ public class Author {
 			public void actionPerformed(ActionEvent arg0) {
 				
 				frmAuthor.setVisible(false);
-				NewSubmission.NewSubmission();
+				//NewSubmission.NewSubmission(); //commented out for now while code is being developed
 			}
 			
 		});
